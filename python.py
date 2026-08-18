c757645f51ea7579d3849da0f3963a2f218f5cdf
Thonne Twange <ttwange2@gmail.com>
2026-08-18 20:52:38 +0300

    learning python from basics

---
+++ python.py
@@ -0,0 +1,30 @@
+print("My learning Path:\n\t- Python Basics\n\t- Data Engineering\n\t- AI")
+
+#Dynamic variables
+info_mail = "infotext@gmail.com"
+support_mail = "support@gmail.com"
+website = "dataworld.com"
+
+print(f"""
+      Welcome to {website} where learning is fun. Incase you need more information
+      kindly email the request to {info_mail} and incase of support with our programs
+      email: {support_mail}""")
+
+# user inputs to a program
+
+name = input("Enter your name:: ")
+
+print(f"My name is::{name}")
+
+# python data types
+#single value
+int  = 20
+float = 4.2
+str = 'yeh'
+bool = False
+
+#multi-values
+list = [[1],[2],[3]]
+set = {[2],[5]}
+tuple = ([6],[8])
+dict =  {['u':4],['d':7]}
