88e25cd578a9245eb4572285b08861b6b6415a67
Thonne Twange <ttwange2@gmail.com>
2026-08-18 21:43:54 +0300

    data types

--- python.py
+++ python.py
@@ -1,3 +1,6 @@
+from os import replace
+
+
 print("My learning Path:\n\t- Python Basics\n\t- Data Engineering\n\t- AI")
 
 #Dynamic variables
@@ -22,9 +25,83 @@ int  = 20
 float = 4.2
 str = 'yeh'
 bool = False
-
+h = None # no value or nothing - used ot show absence on any data
+i = "" # is a string with no characters inside not a None
+o = " " # is a string with white space
 #multi-values
 list = [[1],[2],[3]]
 set = {[2],[5]}
 tuple = ([6],[8])
 dict =  {['u':4],['d':7]}
+
+a = 29
+
+
+# data types, functions and methods
+# functions types
+
+# standalone functions like 
+print()
+
+# methods of class such as 
+replace()
+
+# operations - magic methods
++ - == < >
+
+
+#user defined functions
+
+defined by user for a specific case
+
+# 3rd party functions
+such as tensorflow
+
+# standard built-in module: 
+
+# functions: independent block of code function_name(value)
+#  methods: functions belong to objects/classes value.method_name()
+
+text = "hi"
+number = 10
+
+print(text)
+print(number)
+
+print(type(text))
+print(type(number))
+
+print(len(text))
+print(len(number))
+
+text.upper()
+number.bit_length()
+
+#challenge
+
+age = 94
+height = 75
+name = "Jay"
+are_you_a_student = False
+student_debt = None
+
+#print values
+print(age)
+print(height)
+print(name)
+print(are_you_a_student)
+print(student_debt)
+
+#data types
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(are_you_a_student))
+print(type(student_debt))
+
+#length
+print(len(age))
+print(len(height))
+print(len(name))
+print(len(are_you_a_student))
+print(len(student_debt))
